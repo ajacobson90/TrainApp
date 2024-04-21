957214aea715b9d92d612eeada4c10e9f854b1e2
ajacobson90 <ajacobson@kingsleyassociates.com>
2024-04-21 15:58:19 -0400

    refactored TrainRepository

--- TrainApp/Data/TrainRepository.cs
+++ TrainApp/Data/TrainRepository.cs
@@ -7,17 +7,12 @@ namespace TrainApp.Data
 {
     public class TrainRepository
     {
-        private DateTimeFormatInfo _timeFormat;
         //since there wasn't a strict requirement for persisting data, and I'm not sure about modifying the original text file,
         //I'm deciding to store updates to the data exclusively in memory. Will work fine as long as repository
         //is injected as a singleton during the time the site runs. Oviously we wouldn't do this in a production app.
         private Dictionary<string, Train> _trainData;
         public TrainRepository()
         {
-            DateTimeFormatInfo timeFormat = CultureInfo.CreateSpecificCulture("en-US").DateTimeFormat;
-            timeFormat.TimeSeparator = ".";
-            
-            _timeFormat = timeFormat;
             _trainData = new Dictionary<string, Train>();
         }
         public IEnumerable<Train> GetAll()
@@ -29,23 +24,31 @@ namespace TrainApp.Data
 
         public Train Get(string destination)
         {
+            if (!_trainData.Any())
+                PopulateDataStore();
             return _trainData[destination];
         }
 
         public void Update(Train train)
         {
-            _trainData[train.Destination] = train;
+            if (_trainData.ContainsKey(train.Destination))
+                _trainData[train.Destination] = train;
         }
 
         private void PopulateDataStore()
         {
+            _trainData.Clear();
+
+            DateTimeFormatInfo timeFormat = CultureInfo.CreateSpecificCulture("en-US").DateTimeFormat;
+            timeFormat.TimeSeparator = ".";
+
             using (var reader = new StreamReader("./Train Data.txt"))
             {
                 var line = reader.ReadLine();
                 while (line != null)
                 {
                     var data = line.Split(',');
-                    var time = TimeOnly.Parse(data[0], _timeFormat);
+                    var time = TimeOnly.Parse(data[0], timeFormat);
                     var destination = data[1];
                     var tickets = int.Parse(data[2]);
 
